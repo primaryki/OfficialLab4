737cd858b870a7d3d792d338fe9bb745e1d1db60
PrimaryKi ;D <51946019+primaryki@users.noreply.github.com>
2023-10-02 01:31:16 -0400

    Added transitions and rounded corners - transitions need visual tweak

--- app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -2,13 +2,18 @@ package com.codepath.articlesearch
 
 import android.content.Context
 import android.content.Intent
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import androidx.core.util.Pair
+
 
 const val ARTICLE_EXTRA = "ARTICLE_EXTRA"
 private const val TAG = "ArticleAdapter"
@@ -45,17 +50,25 @@ class ArticleAdapter(private val context: Context, private val movies: List<Movi
             titleTextView.text = movie.title //TODO change to movie title
 //            abstractTextView.text = movie.firstAirDate //TODO comment this out
 
+            val radius = 10; // corner radius, higher value = more rounded
+            val margin = 50; // crop margin, set to 0 for corners with no crop
             Glide.with(context)
                 .load(movie.mediaImageUrl)
+                .circleCrop() // scale image to fill the entire ImageView
+                .transform(RoundedCorners(margin))
                 .into(mediaImageView)
         }
         override fun onClick(v: View?) {
+            val activity = context as MainActivity
             // TODO: Get selected article
             val article = movies[absoluteAdapterPosition]
             // TODO: Navigate to Details screen and pass selected article
             val intent = Intent(context, DetailActivity::class.java)
             intent.putExtra(ARTICLE_EXTRA, article)
-            context.startActivity(intent)
+            val p1 = Pair<View, String>(mediaImageView, "movie")
+            val p2 = Pair<View, String>(titleTextView, "title")
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(activity, p1, p2)
+            context.startActivity(intent, options.toBundle())
         }
     }
 }
